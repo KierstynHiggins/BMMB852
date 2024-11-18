a64b669946257dff88e5ebcd68142ac69981a7c0
KierstynHiggins <129321441+KierstynHiggins@users.noreply.github.com>
2024-11-18 11:51:19 -0500

    Create variant_effect_prediction.mk

---
+++ Lecture12/variant_effect_prediction.mk
@@ -0,0 +1,99 @@
+# BMMB 852: HW- Variant Effect Prediction
+# Kierstyn Higgins
+# 11/17/24
+
+# Accession number of the Staphylococcus aurelis genome.
+ACC=GCF_000013425.1
+
+# The reference file.
+REF=refs/staph-2006.fa
+
+# The GFF file.
+GFF=refs/staph-2006.gff
+
+# The sequencing read accession number.
+SRR=ERR13649352
+
+# The number of reads to get
+N=5000
+
+# The name of the sample (see: bio search ERR13649352)
+SAMPLE=SAMEA115926234
+
+# The path to read 1
+R1=reads/${SAMPLE}_1.fastq
+
+# The path to read 2
+R2=reads/${SAMPLE}_2.fastq
+
+# The resulting BAM file.
+BAM=bam/${SAMPLE}.bam
+
+# The resulting variant VCF file (compressed!).
+VCF=vcf/${SAMPLE}.vcf.gz
+
+# Custom makefile settings.
+SHELL = bash
+.ONESHELL:
+.SHELLFLAGS = -eu -o pipefail -c
+.DELETE_ON_ERROR:
+MAKEFLAGS += --warn-undefined-variables
+MAKEFLAGS += --no-builtin-rules
+#____________________________________________________________________________________
+# Print the usage of the makefile.
+usage:
+	@echo "#"
+	@echo "# SNP call demonstration"
+	@echo "#"
+	@echo "# ACC=${ACC}"
+	@echo "# SRR=${SRR}"
+	@echo "# SAMPLE=${SAMPLE}"
+	@echo "# BAM=${BAM}"
+	@echo "# VCF=${VCF}"
+	@echo "#"
+	@echo "# make bam|vcf|snpeff|all"
+	@echo "#"
+
+# Check that the bio toolbox is installed.
+CHECK_FILE = src/run/genbank.mk
+${CHECK_FILE}:
+	@echo "#"
+	@echo "# Please install toolbox with: bio code"
+	@echo "#"
+	@exit 1
+
+# Create the BAM alignment file.
+bam: ${CHECK_FILE}
+	# Get the reference genome and the annotations.
+	make -f src/run/genbank.mk ACC=${ACC} REF=${REF} GFF=${GFF} fasta gff
+
+	# Index the reference genome.
+	make -f src/run/bwa.mk REF=${REF} index
+
+	# Download the sequence data.
+	make -f src/run/sra.mk SRR=${SRR} R1=${R1} R2=${R2} N=${N} run
+
+	# Align the reads to the reference genome. 
+	# Use a sample name in the readgroup.
+	make -f src/run/bwa.mk SM=${SAMPLE} REF=${REF} R1=${R1} R2=${R2} BAM=${BAM} run stats
+
+# Call the SNPs in the resulting BAM file.
+vcf:
+	make -f src/run/bcftools.mk REF=${REF} BAM=${BAM} VCF=${VCF} run
+
+# Run all the steps.
+all: bam vcf snpeff
+
+# Remove all the generated files.
+clean:
+	rm -rf ${REF} ${GFF} ${R1} ${R2} ${BAM} ${VCF}
+
+# Run variant effect prediction target using snpEff software.
+snpeff:
+	# Build the custom database
+	make -f src/run/snpeff.mk GFF=${GFF} REF=${REF} build
+
+	# Run snpEff using the custom database
+	make -f src/run/snpeff.mk GFF=${GFF} REF=${REF} VCF=${VCF} run
+# These targets do not correspond to files.
+.PHONY: bam vcf all usage clean snpeff	
